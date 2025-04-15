157ece0b384f77b1efa9af8963d6526017d6b471
Eugene <eugene.chege@student.moringaschool.com>
2025-04-15 18:04:16 +0300

    update todo=13

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,50 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 import Button from './components/Button';
-import Todos from './components/Todos';7
+import Todos from './components/Todos';
 import { TODOS } from './data/data';
 
 function App() {
-
   const [todos, setTodos] = useState(TODOS);
+  const [newTodo, setNewTodo] = useState('');
 
-  console.log(todos);
-  
-  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    
-  }
 
+    const trimmedTodo = newTodo.trim();
+    if (!trimmedTodo) return; 
+
+    const newTask = {
+      id: Date.now(), 
+      task: trimmedTodo,
+      completed: false,
+    };
+
+    setTodos([newTask, ...todos]);
+    setNewTodo(''); 
+  };
 
   return (
-   <div>
+    <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="todo" />
-        <button type='submit'>Add Todo</button>
+        <input
+          type="text"
+          name="todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter a new todo"
+        />
+        <button type="submit">Add Todo</button>
       </form>
 
-      <h2>My todos</h2>
+      <h2>My Todos</h2>
       <Todos todos={todos} setTodos={setTodos} />
+
       <Button />
       <Button greeting="jambo" />
       <Button greeting="konichiwa" />
-
-   </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
